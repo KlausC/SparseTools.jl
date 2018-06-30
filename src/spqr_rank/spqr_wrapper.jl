9d2bd637c003d8981b40c75b122e8a9044697e50
Klaus Crusius <klaus.crusius@web.de>
2018-06-30 13:02:38 +0200

    fixed spqr_wrapper

--- src/spqr_rank/spqr_wrapper.jl
+++ src/spqr_rank/spqr_wrapper.jl
@@ -1,5 +1,5 @@
 """
-    Q, R, C, pcol, prow, info = spqr_wrapper(A, B, tol, get_details)
+    Q, R, C, prow, pcol, info = spqr_wrapper(A, B, tol, get_details)
 
 SPQR_WRAPPER wrapper around spqr to get additional statistics.
 The ouput `R` has reduced rown count, if final diagonal elements would become below `tol`.
@@ -24,24 +24,27 @@ function spqr_wrapper(A::AbstractArray{T}, B::AbstractArray{T}, tol, get_details
     function _spqr_wrapper(A::AbstractSparseArray, tol)
         m, n = size(A)
         qrf = qr(A, tol=tol)
-        Q, R, pcol, prow = qrf.Q, qrf.R, qrf.pcol, qrf.prow
-        rk = size(R, 1)
-        norm_E_fro = 0.0 #norm(A[prow,pcol] - Q * [R; spzeros(m-rk,n)], 2) 
-        Q, R, pcol, prow, maximum(R.rowval), norm_E_fro
+        Q, R, prow, pcol = qrf.Q, qrf.R, qrf.prow, qrf.pcol
+        rk = maximum(R.rowval)
+        B = Matrix(A)
+        norm_E_fro = errornorm(Q, A, rk, prow, pcol)
+        Q, R, prow, pcol, rk, norm_E_fro
     end
 
     function _spqr_wrapper(A::AbstractArray, tol)
         qrf = qr(A, Val(true)) # enable column pivoting
-        Q, R, pcol, prow = qrf.Q, qrf.R, qrf.p, [1:size(A, 1)...]
+        Q, R, prow, pcol = qrf.Q, qrf.R, [1:size(A, 1)...], qrf.p
+        rm, rn = size(R)
         k = findlast(gt(tol), diag(R))
-        rank = k == nothing ? 0 : k
-        R[k+1:end,:] = 0
+        rk = k == nothing ? 0 : k
+        norm_E_fro = norm(view(R, k+1:rm, k+1:rn))
+        R[k+1:end,:] .= 0
         # compute Q*R = A[prow,pcol]
-        norm_E_fro = norm(A[prow,pcol] - Q * R, 2) 
-        Q, R, prow, pcol, rank, norm_E_fro
+        norm_E_fro1 = norm(A[prow,pcol] - Q * R, 2)
+        Q, R, prow, pcol, rk, norm_E_fro
     end
 
-    Q, R, pcol, prow, rank, norm_E_fro = _spqr_wrapper(A, tol)
+    Q, R, prow, pcol, rank, norm_E_fro = _spqr_wrapper(A, tol)
     R = striprows(R, rank)
 
     if isempty(B)
@@ -55,7 +58,7 @@ function spqr_wrapper(A::AbstractArray{T}, B::AbstractArray{T}, tol, get_details
     t = get_details != 0 ? time_ns() - t : t
     info = (rank_A_estimate=rank, norm_E_fro=norm_E_fro, time=t)
 
-    Q, R, C, pcol, prow, info
+    Q, R, C, prow, pcol, info
 end
 
 function striprows(A::SparseMatrixCSC, k::Int)
@@ -75,4 +78,34 @@ function striprows(A::AbstractMatrix, k::Int)
         A
     end
 end
+
+"""
+    Calculate `norm((Q' * A[prow,pcol])[k+1:end,k+1:end])` 
+"""
+function errornorm(Q::AbstractMatrix, A::AbstractMatrix{T}, k::Int, prow::Vector{Int}, pcol::Vector{Int}) where T<:Number
+
+    m, n = size(A)
+    B = Vector{T}(undef, m)
+    sum = real(T)(0)
+    if k < m
+        for i = k+1:n
+            copyto!(B, view(A, prow, pcol[i]))
+            if norm(B, Inf) != 0
+                lmul!(Q', B)
+                j = k
+                while j < m
+                    j += 1
+                    sum += abs2(B[j])
+                end
+            end
+        end
+    end
+    sqrt(sum)
+end
+
+
+
+
+
+
 
